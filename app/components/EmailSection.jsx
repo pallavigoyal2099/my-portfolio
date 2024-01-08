ba531a8a7564337d2df3584c9c1e490195124aa1
Pallavi Goyal <pallavigoyal2099@gmail.com>
2024-01-08 23:42:28 +0530

    improved css

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -59,7 +59,7 @@ const EmailSection = () => {
   return (
     <section
       id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative z-0"
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -49,7 +49,10 @@ const Navbar = () => {
             </button>
           )}
         </div>
-        <div className="menu hidden md:block md:w-auto" id="navbar">
+        <div
+          className="menu  bg-opacity-5 hidden md:block md:w-auto"
+          id="navbar"
+        >
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
               <li key={index}>
