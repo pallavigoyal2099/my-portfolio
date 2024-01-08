ea0fe610f61b6598659d6bd2b47cb4d36e59ace4
Pallavi Goyal <pallavigoyal2099@gmail.com>
2024-01-08 22:14:20 +0530

    improved mobile view

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -83,7 +83,7 @@ const Projects = () => {
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
-      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6 flex-wrap ">
         <ProjectTag
           onClick={handleTagChange}
           name="All"
